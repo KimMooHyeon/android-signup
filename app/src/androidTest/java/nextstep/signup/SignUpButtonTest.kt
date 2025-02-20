9fbdc2fdde7e8a417532468f3df4a0a032e6055c
김무현 <2230198@ad.h-firework.com>
2025-02-20 18:33:33 +0900

    test: State 클래스로 리팩토링 후 테스트 정상동작 확인

--- app/src/androidTest/java/nextstep/signup/SignUpButtonTest.kt
+++ app/src/androidTest/java/nextstep/signup/SignUpButtonTest.kt
@@ -1,8 +1,11 @@
 package nextstep.signup
 
+import android.app.Application
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.test.core.app.ApplicationProvider
+import nextstep.signup.signup.model.SignUpState
 import nextstep.signup.signup.component.SignUpContents
 import org.junit.Rule
 import org.junit.Test
@@ -14,23 +17,19 @@ class SignUpButtonTest {
 
     @Test
     fun 비밀번호_인풋_비어있을때_버튼_비활성화_확인() {
-        // given when
+        // when
+        val signUpState =
+            SignUpState(context = ApplicationProvider.getApplicationContext<Application>()).apply {
+                updateName("김무현")
+                updateEmail("dlfb7@naver.com")
+                updatePassword("")
+                updatePasswordConfirm("12345678")
+            }
+        // given
         composeTestRule.setContent {
             SignUpContents(
-                nameInputText = "Philip Vincent",
-                nameErrorMessage = "Sherri Walsh",
-                emailInputText = "calvin.allen@example.com",
-                emailErrorMessage = "",
-                passwordInputText = "voluptaria",
-                passwordErrorMessage = "",
-                passwordConfirmInputText = "",
-                passwordConfirmErrorMessage = "",
-                buttonIsEnabled = false,
+                signUpState = signUpState,
                 onShowSnackbar = {},
-                onNameValueChange = {},
-                onEmailValueChange = {},
-                onPasswordValueChange = {},
-                onPasswordConfirmValueChange = {}
             )
         }
 
@@ -41,23 +40,19 @@ class SignUpButtonTest {
 
     @Test
     fun 이메일_형식_안맞을때_버튼_비활성화_확인() {
-        // given when
+        // when
+        val signUpState =
+            SignUpState(context = ApplicationProvider.getApplicationContext<Application>()).apply {
+                updateEmail("dlfb7naver.com")
+                updateName("김무현")
+                updatePassword("12345678")
+                updatePasswordConfirm("12345678")
+            }
+        //given
         composeTestRule.setContent {
             SignUpContents(
-                nameInputText = "Philip Vincent",
-                nameErrorMessage = "",
-                emailInputText = "calvin.allen",
-                emailErrorMessage = "",
-                passwordInputText = "voluptaria",
-                passwordErrorMessage = "",
-                passwordConfirmInputText = "",
-                passwordConfirmErrorMessage = "",
-                buttonIsEnabled = false,
+                signUpState = signUpState,
                 onShowSnackbar = {},
-                onNameValueChange = {},
-                onEmailValueChange = {},
-                onPasswordValueChange = {},
-                onPasswordConfirmValueChange = {}
             )
         }
 
@@ -67,23 +62,19 @@ class SignUpButtonTest {
 
     @Test
     fun 패스워드_매치_안될때_버튼_비활성화_확인() {
-        // given when
+        // when
+        val signUpState =
+            SignUpState(context = ApplicationProvider.getApplicationContext<Application>()).apply {
+                updateEmail("dlfb7naver.com")
+                updateName("김무현")
+                updatePassword("4567891")
+                updatePasswordConfirm("12345678")
+            }
+        //given
         composeTestRule.setContent {
             SignUpContents(
-                nameInputText = "Philip Vincent",
-                nameErrorMessage = "",
-                emailInputText = "calvin.allen",
-                emailErrorMessage = "",
-                passwordInputText = "12345678",
-                passwordErrorMessage = "",
-                passwordConfirmInputText = "123456789",
-                passwordConfirmErrorMessage = "",
-                buttonIsEnabled = false,
+                signUpState = signUpState,
                 onShowSnackbar = {},
-                onNameValueChange = {},
-                onEmailValueChange = {},
-                onPasswordValueChange = {},
-                onPasswordConfirmValueChange = {}
             )
         }
 
@@ -93,23 +84,19 @@ class SignUpButtonTest {
 
     @Test
     fun 모든_입력필드_정상입력시_버튼_활성화_확인() {
-        // given when
+        // when
+        val signUpState =
+            SignUpState(context = ApplicationProvider.getApplicationContext<Application>()).apply {
+                updateEmail("dlfb7naver.com")
+                updateName("김무현")
+                updatePassword("4567891")
+                updatePasswordConfirm("12345678")
+            }
+        // given
         composeTestRule.setContent {
             SignUpContents(
-                nameInputText = "김무현",
-                nameErrorMessage = "",
-                emailInputText = "dlfb7@naver.com",
-                emailErrorMessage = "",
-                passwordInputText = "12345678",
-                passwordErrorMessage = "",
-                passwordConfirmInputText = "12345678",
-                passwordConfirmErrorMessage = "",
-                buttonIsEnabled = false,
+                signUpState = signUpState,
                 onShowSnackbar = {},
-                onNameValueChange = {},
-                onEmailValueChange = {},
-                onPasswordValueChange = {},
-                onPasswordConfirmValueChange = {}
             )
         }
 
